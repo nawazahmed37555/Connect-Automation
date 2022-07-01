ccfcd7574e16ac8f9b2f300d0440a18705eb69f5
admin <admin@44.175.0.192>
2022-07-01 10:50:24 +0530

    Updated runner.java

--- Connect_Automation/src/test/java/runner/Runner.java
+++ Connect_Automation/src/test/java/runner/Runner.java
@@ -15,7 +15,7 @@ import utils.SQLConnector;
  	 features = "src/test/resources/features/",
  		 glue = "stepDefinitions",  
  		 monochrome = true,
- 		 tags = "@Users",
+ 		 tags = "@Login/Logout",
  		 plugin = {"pretty", "html:target/cucumber/report.html", "json:target/cucumber.json", "html:target/selenium-reports/selenium-reports.html"}
 )
 public class Runner extends AbstractTestNGCucumberTests {	 
